a53f4dbb5c8c67f42b8a26ec66bc3c785c0b676a
alpacawool <44758340+alpacawool@users.noreply.github.com>
2020-10-23 22:36:55 -0700

    Initial files

---
+++ ConsoleApplication1/ConsoleApplication1/README.txt
@@ -0,0 +1,8 @@
+The following command will compile code:
+gcc --std=gnu99 -o movies_by_year main.c
+------------------------
+After successful compilation to run:
+./movies_by_year
+
+-------------------------
+For any questions, please email boothpat@oregonstate.edu
---
+++ ConsoleApplication1/ConsoleApplication1/main.c
@@ -0,0 +1,27 @@
+/********************************************************************
+* Program Name: Assignment 3: smallsh
+* Author: Patricia Booth
+* OSU Email Address: boothpat@oregonstate.edu
+* Course Number / Section: CS 344-400
+* Due Date: 11/03/2020
+* Last Modified: 10/23/2020
+* Description: 
+* Citations:
+*
+*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/********************************************************************
+* Main Function
+*********************************************************************/
+
+int main(int argc, char *argv[]) {
+	
+	printf("hello world!");
+
+	return EXIT_SUCCESS;
+}
