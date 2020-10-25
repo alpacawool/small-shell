bfe92f9226c19b36dbd7550cda4aa15f3ab12f57
alpacawool <44758340+alpacawool@users.noreply.github.com>
2020-10-24 22:48:48 -0700

    Added variable expansion for $$ commands

--- ConsoleApplication1/ConsoleApplication1/main.c
+++ ConsoleApplication1/ConsoleApplication1/main.c
@@ -17,106 +17,11 @@
 #include <dirent.h>
 // getcwd() - Current working directory
 #include <unistd.h>
+// Processes
+#include <sys/types.h>
 
 #define MAX_CHAR 2048
 
-/********************************************************************
-* INSTRUCTIONS
-*
-* [1] THE COMMAND PROMPT ------------------------------------
-*
-* Use the colon : symbol as a prompt for each command line. 
-* The general syntax of a command line is:
-* command [arg1 arg2 ...] [< input_file] [> output_file] [&]
-* …where items in square brackets are optional.
-*
-* * You can assume that a command is made up of words
-*   separated by spaces.
-* * The special symbols , and & are recognized, but they must be
-* * surrounded by spaces like other words.
-*
-* * If the command is to be executed in the background, the last word 
-* * must be &. If the & character appears anywhere else, just 
-* * treat it as normal text.
-*
-* * If standard input or output is to be redirected, the > or < words
-* * followed by a filename word must appear after all the arguments.
-* * Input redirection can appear before or after output redirection.
-*
-* * Your shell does not need to support any quoting; so arguments with 
-* * spaces inside them are not possible. We are also not implementing
-* * the pipe "|" operator.
-*
-* * Your shell must support command lines with a maximum length of 2048 
-* * characters, and a maximum of 512 arguments.
-*
-* * You do not need to do any error checking on the syntax
-* * of the command line.
-*
-*
-* [2] COMMENTS & BLANK LINES ------------------------------
-*
-* Your shell should allow blank lines and comments.
-*
-* Any line that begins with the # character is a comment line
-* and should be ignored. Mid-line comments, such as the C-style //,
-* will not be supported.
-*
-* A blank line (one without any commands) should also do nothing.
-*
-* Your shell should just re-prompt for another command when it
-* receives either a blank line or a comment line.
-*
-*
-* [3] EXPANSION OF VARIABLE $$ ------------------------------
-*
-* Your program must expand any instance of "$$" in a command into 
-* the process ID of the smallsh itself. Your shell does not otherwise 
-* perform variable expansion. 
-*
-*
-* [4] BUILT-IN COMMANDS --------------------------------------
-*
-* Your shell will support three built-in commands: exit, cd, 
-* and status. These three built-in commands are the only ones that 
-* your shell will handle itself - all others are simply passed on 
-* to a member of the exec() family of functions.
-*
-* You do not have to support input/output redirection for these
-* built in commands
-*
-* These commands do not have to set any exit status.
-*
-* If the user tries to run one of these built-in commands in the 
-* background with the & option, ignore that option and run the command 
-* in the foreground anyway (i.e. don't display an error, just run the 
-* command in the foreground).
-*
-* exit
-* The exit command exits your shell. It takes no arguments. 
-* When this command is run, your shell must kill any other processes 
-* or jobs that your shell has started before it terminates itself.
-*
-* cd
-* The cd command changes the working directory of smallsh.
-* By itself - with no arguments - it changes to the directory 
-* specified in the HOME environment variable
-* This is typically not the location where smallsh was executed from,
-* unless your shell executable is located in the HOME directory, 
-* in which case these are the same.
-* This command can also take one argument: the path of a directory to
-* change to. Your cd command should support both absolute and relative
-* paths.
-*
-* status
-* The status command prints out either the exit status or the 
-* terminating signal of the last foreground process ran by your shell.
-* If this command is run before any foreground command is run, then it 
-* should simply return the exit status 0.
-* The three built-in shell commands do not count as foreground processes
-* for the purposes of this built-in command - i.e., status should
-* ignore built-in commands.
-*********************************************************************/
 
 /********************************************************************
 * Function: printColon
@@ -129,6 +34,68 @@ void printColon() {
 }
 
 /********************************************************************
+* Function: expandVariable
+* Receives:
+* Returns:
+* Description:
+* Citations: 
+* Exploration: Process Concept & States
+* https://repl.it/@cs344/42pidc
+* StackOverflow question on converting integers to strings
+* https://stackoverflow.com/questions/8257714/
+* Find and replace substrings in C
+* https://stackoverflow.com/questions/779875/
+*********************************************************************/
+char * expandVariable(char * targetString) {
+
+	char * expandedString;
+	char * tempPtr;
+	char * insertPtr;
+	int lengthDistance;
+	int count;
+
+	char * variableProcess = "$$";
+
+	// Get process ID
+	int currentProcess = getpid();
+
+	// Convert process ID to string
+	char * processString = malloc(sizeof(char) * MAX_CHAR);
+	sprintf(processString, "%d", currentProcess);
+
+	// Get length of variable and expansion
+	int variableLength = strlen(variableProcess);
+	int expandLength = strlen(processString);
+
+	// Count how many replacements
+	insertPtr = targetString;
+	for (count = 0; tempPtr = strstr(insertPtr, variableProcess); ++count) {
+		insertPtr = tempPtr + variableLength;
+	}
+	tempPtr
+		= expandedString
+		= malloc(strlen(targetString)
+			+ (expandLength - variableLength) * count + 1);
+
+	// Find and replace and build final string
+	while (count--) {
+		insertPtr = strstr(targetString, variableProcess);
+		lengthDistance = insertPtr - targetString;
+		tempPtr = strncpy(tempPtr, targetString, lengthDistance)
+			+ lengthDistance;
+		tempPtr = strcpy(tempPtr, processString) + expandLength;
+		targetString += lengthDistance + variableLength;
+	}
+	strcpy(tempPtr, targetString);
+
+	// Free memory
+	free(processString);
+
+
+	return expandedString;
+}
+
+/********************************************************************
 * Function: getString
 * Receives: none
 * Returns: Pointer to char
@@ -138,6 +105,8 @@ void printColon() {
 *                       later point.
 * Citation: Stack Overflow question on getting string pointers via
 *           https://stackoverflow.com/questions/14707427/
+* Stack Overflow question on finding substrings
+*           https://stackoverflow.com/questions/12784766/ 
 *********************************************************************/
 char * getString() {
 	char *stringVal;
@@ -148,12 +117,20 @@ char * getString() {
 	//scanf("%s", stringVal);
 	fgets(stringVal, MAX_CHAR, stdin);
 
+	// See if string contains $$
+	if (strstr(stringVal, "$$") != NULL) {
+		// Variable expansion of $$ into process ID
+		char * expandedVal;
+		expandedVal = expandVariable(stringVal);
+		free(stringVal);
+		stringVal = expandedVal;
+	}
+
 	// Remove trailing newline
 	// https://stackoverflow.com/questions/2693776
 	if (strlen(stringVal) > 1) {
 		stringVal[strcspn(stringVal, "\n")] = 0;
 	}
-
 
 	return stringVal;
 }
@@ -251,6 +228,7 @@ void requestInputLoop() {
 	// While user has not entered exit command
 	while (strstr("exit", userString) == NULL) {
 
+		printf("userString: %s\n", userString);
 		// Process command
 		processCommand(userString);
 
